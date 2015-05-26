947c5b43adb00b9ee5f8df9efdb9d9bbdf47a838
Toni Dermawan <toni@erudeye.co.id>
2015-05-27 00:48:10 +0700

    Demo 1-E * no view

---
+++ SiRePa/Controllers/NoviewController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiRePa.Controllers
+{
+    public class NoviewController : Controller
+    {
+        // GET: Noview
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
