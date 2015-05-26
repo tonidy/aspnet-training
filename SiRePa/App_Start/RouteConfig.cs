e6c185e3135d0f1c9a27fa8ef567f671ab010017
Toni Dermawan <toni@erudeye.co.id>
2015-05-27 00:27:19 +0700

    Demo 1-B

--- SiRePa/App_Start/RouteConfig.cs
+++ SiRePa/App_Start/RouteConfig.cs
@@ -13,11 +13,19 @@ namespace SiRePa
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+
+            routes.MapRoute(
+               name: "bpjs_route",
+               url: "bpjs/{controller}/{action}/{id}",
+               defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
         }
     }
 }
